b923a0efc656b0fe180424b3167e6a9eb64a522f
Sagar Jha <96566831+sagarkumar0202@users.noreply.github.com>
2025-05-25 12:20:58 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
+
 
 st.title("Diabetes Prediction App")
 
@@ -10,14 +10,6 @@ st.write(data)
 x = data[['Age', 'Glucose_Level']]
 y = data['Diabetes'].map({'No': 0, 'Yes': 1})
 
-model = LogisticRegression()
-model.fit(x, y)
 
-age = st.number_input("Enter Age:", 18.0, 100.0, step=1.0)
-glucose = st.number_input("Enter Glucose Level:", 50.0, 200.0, step=1.0)
 
-prediction = model.predict([[age, glucose]])[0]
-result = "Yes" if prediction == 1 else "No"
-
-st.write("Diabetes Prediction:", result)
 
