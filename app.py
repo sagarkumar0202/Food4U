ab4c5798f5243e9d0ad60dff71ffe204752e2862
Sagar Jha <96566831+sagarkumar0202@users.noreply.github.com>
2025-06-23 21:41:15 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,23 +1,21 @@
 import streamlit as st
-import pandas as pd
-from sklearn.linear_model import LogisticRegression
+from textblob import TextBlob
 
-st.title("Diabetes Prediction Apps")
+text = "I really enjoy using this app. It's fantastic!"
+blob = TextBlob(text)
+print(blob.sentiment)
 
-data = pd.read_csv("diabetes_data.csv")
-st.write(data)
+st.title("Sentiment Analyzer")
 
-x = data[['Age', 'Glucose_Level']]
-y = data['Diabetes'].map({'No': 0, 'Yes': 1})
+text = st.text_area("Enter your text here")
 
-model = LogisticRegression()
-model.fit(x, y)
+if st.button("Analyze"):
+    blob = TextBlob(text)
+    polarity = blob.sentiment.polarity
 
-age = st.number_input("Enter Age:", 18.0, 100.0, step=1.0)
-glucose = st.number_input("Enter Glucose Level:", 50.0, 200.0, step=1.0)
-
-prediction = model.predict([[age, glucose]])[0]
-result = "Yes" if prediction == 1 else "No"
-
-st.write("Diabetes Prediction:", result)
-
+    if polarity > 0:
+        st.success("Sentiment: Positive 😊")
+    elif polarity < 0:
+        st.error("Sentiment: Negative 😞")
+    else:
+        st.warning("Sentiment: Neutral 😐")
